9b9e72adebeb14a8e98068d7d6f07f988af22569
azdoolittle <drewdoolittle@gmail.com>
2020-05-04 10:03:26 -0400

    adds and prints book in booklist

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        ArrayList<Book> booklist = new ArrayList<Book>();
+
+        String answer = "y";
+        //add a new book
+        //create a book from our object
+        while (answer.equalsIgnoreCase("y")) {
+            System.out.println("Do you want to enter a book?");
+            answer = keyboard.nextLine();
+
+            if (answer.equalsIgnoreCase("y")) {
+                Book book = new Book();
+                System.out.print("Enter the author:");
+                book.setAuthor(keyboard.nextLine());
+                System.out.print("Enter the title: ");
+                book.setTitle(keyboard.nextLine());
+                booklist.add(book);
+            }
+        }
+
+        //print all the books in the array list
+        System.out.println("All the books:");
+
+        for(Book book:booklist)
+        {
+            System.out.print(book.getAuthor());
+            System.out.print(" ");
+            System.out.print(book.getTitle());
+            System.out.println();
+        }
+
+    }
+}
